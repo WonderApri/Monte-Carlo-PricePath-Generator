ff9b1bc8d549b00c7c6d312fd3263ae50030bb75
Aparajita <a.parihar @student.reading.ac.uk>
2025-12-12 13:28:45 +0000

    created matrix to hold values

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ T = 1.0              #total time in yrs
 steps = 252          #steps in simulation (days in year when market open- trading year)
 paths = 10000        #no. of independent price paths to simulate
 
+dt = T / steps           
+Z = np.random.normal(0,1, (paths, steps))   #produces matrix 
+#each row of matrix is one price path's random noises
+
+S = np.zeros((paths, steps + 1))
+S[:, 0] = S_value
 
